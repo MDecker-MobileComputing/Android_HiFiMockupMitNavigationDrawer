8f8004ee8f411370c4c47d33427efa774a01e742
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-05 08:50:04 +0100

    Zustand nach Erzeugung mit Wizard

---
+++ app/src/main/java/de/mide/android/hifimockup/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package de.mide.android.hifimockup.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
