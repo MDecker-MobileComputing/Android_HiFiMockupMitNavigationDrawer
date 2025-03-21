e594580170ffb5cd9c5eadbd4faa886ff507bcad
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-21 09:35:57 +0100

    Update BalkendiagrammFragment.java

--- app/src/main/java/de/mide/android/hifimockup/fragmente/BalkendiagrammFragment.java
+++ app/src/main/java/de/mide/android/hifimockup/fragmente/BalkendiagrammFragment.java
@@ -23,8 +23,7 @@ public class BalkendiagrammFragment extends Fragment {
     @Override
     public View onCreateView( LayoutInflater inflater,
                               ViewGroup container,
-                              Bundle savedInstanceState
-                ) {
+                              Bundle savedInstanceState  ) {
 
         return inflater.inflate( R.layout.fragment_balkendiagramm,
                                  container,
